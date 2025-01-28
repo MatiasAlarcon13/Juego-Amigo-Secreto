23375864ddda7274a3c3495655ded93f6b45f10f
Matias <matyasnicolas13@gmail.com>
2025-01-28 00:30:47 -0300

    Función Sorteo

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let nombres = [];
+let contador = 0;
 
 function agregarAmigo(){
     let inputNombre = document.getElementById("amigo").value;
@@ -9,5 +10,18 @@ function agregarAmigo(){
         console.log(nombres);
         document.querySelector("#amigo").value = "";
         listaAmigos.innerHTML = nombres.join("<br>");
+        contador++;
+    }
+}
+
+function sortearAmigo(){
+    if(contador == 0){
+        alert("No hay ningun amigo para sortear, ingresa nombres de tus amigos para jugar");
+    }else{
+        amigoRandom = parseInt(Math.floor(Math.random() * contador));
+        console.log(nombres[amigoRandom]);
+        let resultado = document.querySelector("#resultado");
+        resultado.innerHTML = "El Amigo Secreto Sorteado es: " + nombres[amigoRandom];
+        listaAmigos.innerHTML = "";
     }
 }
